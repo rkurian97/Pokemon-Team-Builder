5e6af7bd9e6d2db7e5109beaad9151c54bddb639
andersonbuzzidr <77116414+andersonbuzzidr@users.noreply.github.com>
2021-05-21 00:25:14 -0500

    navbar updated

--- server.js
+++ server.js
@@ -15,18 +15,6 @@ const PORT = process.env.PORT || 3001;
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Routes
-// =============================================================
-app.get('/', (req, res) => {
-    res.render('index');
-});
-
-app.get('/search', (req, res) => {
-    res.render('search');
-});
-
-app.get('/team', (req, res) => {
-    res.render('team');
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -36,8 +24,22 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.static("images"));
 
+// Routes
+// =============================================================
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/search", (req, res) => {
+  res.render("search");
+});
+
+app.get("/team", (req, res) => {
+  res.render("team");
+});
+
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, () => {
-    console.log('App listening on PORT ' + PORT);
+  console.log("App listening on PORT " + PORT);
 });
