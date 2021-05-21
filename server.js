cdfbb503baa83b0a5cc970b48206d94f65455904
andersonbuzzidr <77116414+andersonbuzzidr@users.noreply.github.com>
2021-05-20 21:09:12 -0500

    navbar added to handlebars

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 // Dependencies
 // =============================================================
-const express = require('express');
-const exphbs = require('express-handlebars');
+const express = require("express");
+const exphbs = require("express-handlebars");
+const path = require("path");
 // Require the 'express-session' module
-const session = require('express-session')
+const session = require("express-session");
 
 // Sets up the Express App
 // =============================================================
@@ -11,20 +12,26 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Sets Handlebars as the default template engine
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// express.static middleware
+
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static("images"));
 
 // Routes
 // =============================================================
-app.get('/', (req, res) => {
-
-    res.render('index');
-  });
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, () => {
-    console.log('App listening on PORT ' + PORT);
-  });
-  
+  console.log("App listening on PORT " + PORT);
+});
