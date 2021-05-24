d9c974f91d9038dd3c8d3ce11fb3092667d246f6
rkurian <kurian.rohit1229@gmail.com>
2021-05-23 21:25:34 -0500

    added search bar, and routes for login/register

--- db/schema.sql
+++ db/schema.sql
@@ -4,10 +4,19 @@ CREATE DATABASE pokemon_db;
 USE pokemon_db;
 
 CREATE TABLE users(
-    id INTEGER NOT NULL,
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(30),
+    lastname VARCHAR(30),
     email VARCHAR(30),
     password VARCHAR(30),
     name VARCHAR(30)
 )
 
-C
+CREATE TABLE pokemon(
+    userid INTEGER NOT NULL,
+    pokemon VARCHAR(30),
+
+    PRIMARY KEY(userid, pokemon)
+    FOREIGN KEY (userid)
+    REFERENCES users(id)
+)
