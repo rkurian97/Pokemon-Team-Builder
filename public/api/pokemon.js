d3a0c144e313b8be4392763bd6b6fc6e90abca5f
andersonbuzzidr <77116414+andersonbuzzidr@users.noreply.github.com>
2021-05-26 11:04:49 -0500

    search page styled

--- public/api/pokemon.js
+++ public/api/pokemon.js
@@ -18,9 +18,33 @@ function apiCall(pokemon) {
       return response.json();
     })
     .then((data) => {
-      const card = $(`<h1>test </h1>`);
+      const card = $(`
+      <div class="card">
+      <img class="card-img-top" src="${data.sprites.other.dream_world.front_default}" alt="Card image cap">
+      <div class="card-body">
+          <section class="heading">
+              <h1 class="main-heading">${data.forms[0].name}</h1>
+              <p class="stats">HP ${data.stats[0].base_stat} | XP ${data.base_experience}</p>
+          </section>
+         <section class="sub-heading">
+             <div>
+                 <p class="description">${data.types[0].type.name}</p>
+                 <p class="title">Type</p>
+             </div>
+             <div>
+                 <p class="description">${data.weight}hg</p>
+                 <p class="title">Weight</p>
+             </div>
+             <div>
+                 <p class="description">${data.height}dm</p>
+                 <p class="title">Height</p>
+             </div>
+         </section>
+      </div>
+  </div>
+</div>  `);
 
-      $("#card").append(card);
+      $("#card-container").append(card);
       console.log(data);
     });
 }
